92c97650f135976283b4d7bf0d8b1ee1d2f50f26
Jojister <59896961+Jojister@users.noreply.github.com>
2021-03-12 12:06:51 -0500

    changes to triangle.js

--- triangle.js
+++ triangle.js
@@ -1,17 +1,17 @@
-let canvas = document.getElementById('canvasid');
-let ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvasid');
+var ctx = canvas.getContext('2d');
 
 //3 points
 ctx.fillRect(0,0,1,1);
 ctx.fillRect(800,0,1,1);
 ctx.fillRect(400,800,1,1);
 
-let x4 = randomnumber(0, 800);
-let y4 = randomnumber(0, 800);
+var x4 = randomnumber(0, 800);
+var y4 = randomnumber(0, 800);
 
 function midpoint(a, b, h, i){
-    let nx = (a + h)/2
-    let ny = (b + i)/2
+    var nx = (a + h)/2
+    var ny = (b + i)/2
     return [nx, ny];
 };
 
@@ -25,13 +25,13 @@ function randomchoice(arr) {
 
 
 for (let times = 0; times < 100000; times++){
-    let rpy = 0
-    let rpx = randomchoice([0,800,400])
+    var rpy = 0
+    var rpx = randomchoice([0,800,400])
     if (rpx == 0 || rpx == 800){
         rpy = 0;
     } else {
         rpy = 800;
     };
-    let [x4, y4] = midpoint(rpx, rpy, x4, y4)
+    var [x4, y4] = midpoint(rpx, rpy, x4, y4)
     ctx.fillRect(x4,y4,1,1);
 };
